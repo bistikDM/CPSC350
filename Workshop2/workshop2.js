55d8e57a45661cd51aeff22f33c46708f587e928
Daniel Mulyono <danielmulyono@gmail.com>
2017-10-11 18:52:00 +0000

    Ready for submission.

--- Workshop2/workshop2.js
+++ Workshop2/workshop2.js
@@ -51,9 +51,9 @@ app.post('/create-user', async (req, res) =>
 });
 
 //Delete user request.
-app.delete('/delete-user', async (req, res) =>
+app.get('/delete-user', async (req, res) =>
 {
-    var username = req.query.username; //Or req.params -> need to test.
+    var username = req.query.username;
     
     if(!username)
     {
@@ -63,7 +63,7 @@ app.delete('/delete-user', async (req, res) =>
     {
         try
         {
-            await pool.query('DELETE * FROM user_database WHERE user_name = $1', [username]);
+            await pool.query('DELETE FROM user_database WHERE user_name = $1', [username]);
             res.json({status : 'deleted'});
         } catch(e)
         {
@@ -75,7 +75,7 @@ app.delete('/delete-user', async (req, res) =>
 //List user request.
 app.get('/list-users', async (req, res) =>
 {
-    var type = req.query.type
+    var type = req.query.type;
     var userList;
     
     if(type == 'summary')
@@ -83,10 +83,15 @@ app.get('/list-users', async (req, res) =>
         try
         {
             userList = await pool.query('SELECT first_name, last_name FROM user_database');
-            res.json({users : userList});
+            var users = {users : []};
+            for(var x = 0; x < userList.rows.length; x++) 
+            {
+               users.users.push({firstname : userList.rows[x].first_name, lastname : userList.rows[x].last_name});
+            }
+            res.json(users);
         } catch(e)
         {
-            console.log('Error running summary request');
+            console.log('Error running summary request', e);
         }
     }
     else if(type == 'full')
@@ -94,10 +99,15 @@ app.get('/list-users', async (req, res) =>
         try
         {
             userList = await pool.query('SELECT first_name, last_name, user_name, email FROM user_database');
-            res.json({users : userList});
+            var users = {users : []};
+            for(var x = 0; x < userList.rows.length; x++)
+            {
+                users.users.push({firstname : userList.rows[x].first_name, lastname : userList.rows[x].last_name, username : userList.rows[x].user_name, email : userList.rows[x].email});
+            }
+            res.json(users);
         } catch(e)
         {
-            console.log('Error running full request');
+            console.log('Error running full request', e);
         }
     }
     else
@@ -110,8 +120,8 @@ app.get('/list-users', async (req, res) =>
 app.post('/add-workshop', async (req, res) =>
 {
    var title = req.body.title;
-   var date = req.body.date;
-   var location = req.body.location; //Check if this will mess anything up.
+   var date = dateFormat(req.body.date, 'yyyy-mm-dd');
+   var location = req.body.location;
    var maxSeats = req.body.maxseats;
    var instructor = req.body.instructor;
    
@@ -129,9 +139,9 @@ app.post('/add-workshop', async (req, res) =>
                 var checkInstructor = await pool.query('SELECT instructor FROM instructor_database WHERE instructor = $1', [instructor]);
                 if(checkInstructor.rows.length == 0)
                 {
-                    await pool.query('INSERT INTO instructor_database VALUES ($1)', [instructor]);
+                    await pool.query('INSERT INTO instructor_database (instructor) VALUES ($1)', [instructor]);
                 }
-                await pool.query('INSERT INTO workshop_database VALUES ($1, $2, $3, $4, (SELECT instructor_id FROM instructor_database WHERE instructor = $5))', [title, date, location, maxSeats, instructor]);
+                await pool.query('INSERT INTO workshop_database (title, start_date, location, max_seats, instructor_id) VALUES ($1, $2, $3, $4, (SELECT instructor_id FROM instructor_database WHERE instructor = $5))', [title, date, location, maxSeats, instructor]);
                 res.json({status : 'workshop added'});
             }
             else
@@ -140,7 +150,7 @@ app.post('/add-workshop', async (req, res) =>
             }
        } catch(e)
        {
-           console.log('Error running workshop post request');
+           console.log('Error running workshop post request', e);
        }
    }
 });
@@ -150,8 +160,8 @@ app.post('/enroll', async (req, res) =>
 {
     var username = req.body.username;
     var title = req.body.title;
-    var date = req.body.date;
-    var location = req.body.location; //Check if this will mess anything up.
+    var date = dateFormat(req.body.date, 'yyyy-mm-dd');
+    var location = req.body.location;
     
     if(!username || !title || !date || !location)
     {
@@ -164,7 +174,7 @@ app.post('/enroll', async (req, res) =>
             var checkWorkshop = await pool.query('SELECT title FROM workshop_database WHERE title = $1 AND start_date = $2 AND location = $3', [title, date, location]);
             var checkUsername = await pool.query('SELECT user_name FROM user_database WHERE user_name = $1', [username]);
             var maxSeats = await pool.query('SELECT max_seats FROM workshop_database WHERE title = $1 AND start_date = $2', [title, date]);
-            var checkSeats = await pool.query('SELECT COUNT(ed.user_name) FROM enrollment_database AS ed INNER JOIN workshop_database AS wd WHERE ed.workshop_id = wd.workshop_id');
+            var checkSeats = await pool.query('SELECT COUNT(ed.user_name) FROM enrollment_database AS ed INNER JOIN workshop_database AS wd ON ed.workshop_id = wd.workshop_id');
             if(checkWorkshop.rows.length == 0)
             {
                 res.json({status : 'workshop does not exist'});
@@ -173,9 +183,9 @@ app.post('/enroll', async (req, res) =>
             {
                 res.json({status : 'user does not exist'});
             }
-            else if(checkSeats.COUNT(ed.user_name) < maxSeats.max_seats) //Check to see if the calls on both sides are valid.
+            else if(checkSeats.rows[0].count < maxSeats.rows[0].max_seats) //Check to see if the calls on both sides are valid.
             {
-                await pool.query('INSERT INTO enrollment_database VALUES ((SELECT workshop_id FROM workshop_database WHERE title = $1 AND start_date = $2 AND location = $3), $4)', [title, date, location, username]);
+                await pool.query('INSERT INTO enrollment_database (workshop_id, user_name) VALUES ((SELECT workshop_id FROM workshop_database WHERE title = $1 AND start_date = $2 AND location = $3), $4)', [title, date, location, username]);
                 res.json({status : 'user added'});
             }
             else
@@ -184,7 +194,7 @@ app.post('/enroll', async (req, res) =>
             }
         } catch(e)
         {
-           console.log('Error running enroll post request');
+           console.log('Error running enroll post request', e);
         }
     }
 });
@@ -195,8 +205,12 @@ app.get('/list-workshops', async (req, res) =>
     try
     {
         var workshopList = await pool.query('SELECT title, start_date, location FROM workshop_database');
-        dateFormat(workshopList.start_date, 'yyyy-mm-dd');
-        res.json({workshops : [workshopList]});
+        var workshops = {workshops : []};
+        for(var x = 0; x < workshopList.rows.length; x++)
+        {
+            workshops.workshops.push({title : workshopList.rows[x].title, date : dateFormat(workshopList.rows[x].start_date, 'yyyy-mm-dd'), location : workshopList.rows[x].location});
+        }
+        res.json(workshops);
     } catch(e)
     {
         console.log('Error running list workshop request');
@@ -208,7 +222,7 @@ app.get('/attendees', async (req, res) =>
 {
    var title = req.query.title;
    var date = req.query.date;
-   var location = req.query.location; //Check if this will mess anything up.
+   var location = req.query.location;
    
    if(!title || !date || !location)
    {
@@ -218,13 +232,23 @@ app.get('/attendees', async (req, res) =>
    {
        try
        {
-           var attendeeList = await pool.query(
-               'SELECT ud.first_name, ud.last_name FROM user_database AS ud INNER JOIN enrollment_database AS ed WHERE ed.user_name = ud.user_name INNER JOIN ' + 
-               'workshop_database AS wd WHERE ed.workshop_id = (SELECT workshop_id FROM workshop_database WHERE title = $1 AND start_date = $2 AND location = $3)', [title, date, location]);
-            res.json({attendees : attendeeList});
+            var attendeeList = await pool.query(
+               'SELECT ud.first_name, ud.last_name FROM user_database AS ud INNER JOIN enrollment_database AS ed ON ed.user_name = ud.user_name INNER JOIN ' + 
+               'workshop_database AS wd ON ed.workshop_id = (SELECT workshop_id FROM workshop_database WHERE title = $1 AND start_date = $2 AND location = $3)', [title, date, location]);
+            var attendees = {attendees : []};
+            for(var x = 0; x < attendeeList.rows.length; x++)
+            {
+                attendees.attendees.push({firstname : attendeeList.rows[x].first_name, lastname: attendeeList.rows[x].last_name});
+            }
+            res.json(attendees);
        } catch(e)
        {
-           console.log('Error running list attendees request');
+           console.log('Error running list attendees request', e);
        }
    }
+});
+
+app.listen(app.get('port'), () => 
+{
+	console.log('Running');
 });
--- Workshop2/workshop2.sql
+++ Workshop2/workshop2.sql
@@ -10,9 +10,16 @@ CREATE TABLE user_database
     PRIMARY KEY(user_name)
 );
 
+CREATE TABLE instructor_database
+(
+    instructor_id serial NOT NULL,
+    instructor VARCHAR(50),
+    PRIMARY KEY(instructor_id)
+);
+
 CREATE TABLE workshop_database
 (
-    workshop_id serial INT NOT NULL,
+    workshop_id serial NOT NULL,
     title VARCHAR(50) NOT NULL,
     start_date DATE,
     location VARCHAR(25),
@@ -20,13 +27,6 @@ CREATE TABLE workshop_database
     instructor_id INT,
     CONSTRAINT instructor_database_instructor_id_fk FOREIGN KEY(instructor_id) REFERENCES instructor_database(instructor_id),
     PRIMARY KEY(workshop_id)
-);
-
-CREATE TABLE instructor_database
-(
-    instructor_id serial INT NOT NULL,
-    instructor VARCHAR(50),
-    PRIMARY KEY(instructor_id)
 );
 
 CREATE TABLE enrollment_database
@@ -38,5 +38,5 @@ CREATE TABLE enrollment_database
 );
 
 CREATE ROLE workshop2 WITH LOGIN;
-GRANT SELECT, INSERT, DELETE ON user_database, workshop_database, instructor_database, enrollment TO workshop2;
+GRANT SELECT, INSERT, DELETE ON user_database, workshop_database, instructor_database, enrollment_database TO workshop2;
 \password workshop2
